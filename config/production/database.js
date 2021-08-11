0f0c3b7f4ecfe5c3e4d4665c2b49d145d85f2046
trastanechora <trastanechora@gmail.com>
2021-08-11 15:48:47 +0700

    Fix config ssl

--- config/production/database.js
+++ config/production/database.js
@@ -13,9 +13,13 @@ module.exports = ({ env }) => ({
         database: config.database,
         username: config.user,
         password: config.password,
-        ssl: false,
+        ssl: {
+          rejectUnauthorized: false,
+        },
       },
-      options: {},
+      options: {
+        ssl: true,
+      },
     },
   },
 });
