3389061117ec2b78414ceff607da482e65c93d72
trastanechora <trastanechora@gmail.com>
2021-08-11 18:02:40 +0700

    Add console

--- config/production/database.js
+++ config/production/database.js
@@ -1,6 +1,8 @@
 const parse = require("pg-connection-string").parse;
 const config = parse(process.env.DATABASE_URL);
 
+console.warn("config", config);
+
 module.exports = ({ env }) => ({
   defaultConnection: "default",
   connections: {
